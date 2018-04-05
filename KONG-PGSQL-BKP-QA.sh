57648c3157b8639ee4461d42986d259776f66743
karmukis <karmukis@gmail.com>
2018-04-05 14:42:47 -0300

    Adding final scripts

---
+++ KONG-PGSQL-BKP-QA.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###################
+# Variables setting 
+###################
+
+
+HOSTNAME=kong-pgsql.infrastructure.marathon.mesos
+#HOSTIP=$( curl -s localhost:8080/v2/apps/infrastructure/kong-pgsql/tasks | awk -F'"' '{print $14}' )
+HOSTIP=$(nslookup -type=A kong-pgsql.infrastructure.marathon.mesos | grep 'Address: ' | awk -F':' '{print $2}')
+PORT=$(nslookup -type=SRV _kong-pgsql.infrastructure._tcp.marathon.mesos | grep service | awk -F' ' '{print $6}')
+
+
+##################
+# Getting the BKP
+##################
+
+docker run -it --rm --name "KONG-PGSQL-BKP" lapp-dvde004:5000/postgres:9.4 pg_dumpall -h $HOSTNAME -p $PORT -c -U postgres > /tmp/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+
